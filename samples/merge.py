5d5a2bb62208eeb361cb6a83a0f988f3cac5146c
zhengbili <zhengbili@github.com>
2022-12-09 14:42:07 +0000

    update result

--- samples/merge.py
+++ samples/merge.py
@@ -2,9 +2,6 @@ import os
 
 filenames=os.listdir('Second-Stage')
 infos=dict([line.split(', ') for line in open('result2.txt').read()[:-1].split('\n')])
-infos['0477e60c7bebf5a212a2cb11fffe9a73d944e7a54f0065164784a44a36449023']='ip:197.160.114.255'
-infos['b0c5bc59cecf4dac640416a83ef9ff52d34cc38f6f37e19b5ee1cc8b2f64e51f']='ip:97.42.221.62'
-infos['efc237e4c2278922f42db9c44ce54a509bdc7433bf192bc3e70e30b637bf45e9']='ip:138.93.153.70'
 f=open('result.txt','w')
 for filename in filenames:
     if filename in infos:f.write('%s, %s\n'%(filename,infos[filename]))
--- samples/test2.sh
+++ samples/test2.sh
@@ -1,12 +1,18 @@
+#修复win下命令
 export PATH=$PWD:$PATH
+#屏蔽网络访问
+export http_proxy='http://localhost:0'
+export https_proxy='http://localhost:0'
 rm temp2.txt
 cd ../sandbox
 for i in $(ls ../samples/Second-Stage/);do
     echo $i | tee -a ../samples/temp2.txt
     ../samples/preprocessing.py ../samples/Second-Stage/$i temp.ps1
-    res=$(timeout -k 20s 10s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
+    #res=$(timeout -k 20s 15s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
+    res=$(../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
     if [[ $res != *"ip:"* ]];then
-        res=$(IgnoreQuote=True timeout -k 20s 10s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
+        #res=$(IgnoreQuote=True timeout -k 20s 15s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
+        res=$(IgnoreQuote=True ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
     fi
     echo "$res" | tee -a ../samples/temp2.txt
 done
