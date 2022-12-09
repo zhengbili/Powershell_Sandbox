7122243d0afa1172db85e6bc53950baf5979ae23
zhengbili <zhengbili@github.com>
2022-12-09 06:34:44 +0000

    udpate preprocessing

---
+++ samples/preprocessing.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import re
+import base64
+import sys
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+with open(inputFile,'r') as f:
+    string = f.read()
+
+#Char
+pattern = re.compile(r"\$[a-zA-Z0-9_]*\s*=\s*\[Char\[\]\]\((.*?)\)(\n)+(.*?)(\n)+(.*?)out-null", re.I)
+matches = pattern.search(string)
+if matches != None:
+    o = open(outputFile, 'w')
+    o.write(matches.group())
+    exit(0)
+
+#powershell -e
+pattern = re.compile(r"powershell(.*?)-e(.*?)\"(.*?)\"", re.I)
+matches = pattern.findall(string)
+if len(matches) > 0:
+    o = open(outputFile, 'w')
+    o.write(base64.b64decode(matches[0][2]).decode("utf-16"))
+    exit(0)
+
+#anti
+pattern = re.compile(r"if(.*?)(get-random|get-date|false)(.*?){", re.I)
+matches = pattern.findall(string)
+if len(matches) > 0:
+    string = re.sub(pattern, "if($true){", string)
+
+#split
+pattern = re.compile(r".([s|S][p|P][l|L][i|I][t|T])\s*\(\s*'([^']*)'\s*\)")
+matches = pattern.findall(string)
+new_string = ""
+for match in matches:
+    #variable_name = match[0]
+    method_name = match[0]
+    split_string = match[1]
+    new_string = f""
+    for char in split_string:
+        new_string += f" -{method_name.lower()}"+ f" '{char}'"
+    print(new_string)
+if len(matches) > 0:
+    string = re.sub(pattern, new_string, string)
+
+o = open(outputFile, 'w')
+o.write(string)
--- samples/test2.sh
+++ samples/test2.sh
@@ -1,10 +1,14 @@
 export PATH=$PWD:$PATH
+rm temp2.txt
 cd ../sandbox
 for i in $(ls ../samples/Second-Stage/);do
-    echo $i
-    res=$(timeout -k 20s 10s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh ../samples/Second-Stage/$i 2>/dev/null)
+    echo $i | tee -a ../samples/temp2.txt
+    ../samples/preprocessing.py ../samples/Second-Stage/$i temp.ps1
+    res=$(timeout -k 20s 10s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
     if [[ $res != *"ip:"* ]];then
-        res=$(IgnoreQuote=True timeout -k 20s 10s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh ../samples/Second-Stage/$i 2>/dev/null)
+        res=$(IgnoreQuote=True timeout -k 20s 10s ../src/powershell-unix/bin/Debug/net7.0/linux-x64/publish/pwsh temp.ps1 2>/dev/null)
     fi
-    echo "$res"
+    echo "$res" | tee -a ../samples/temp2.txt
 done
+cd ../samples
+./postprocessing.py temp2.txt result2.txt
